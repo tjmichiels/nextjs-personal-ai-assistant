9fc2a00fe9afa63078796f4b1afce2a4120c1f56
Thomas Michiels <thomasmichiels0@gmail.com>
2025-03-27 12:34:04 +0100

    Test commit for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
       <div className="w-full justify-items-center min-h-screen h-full align-middle">
         <h1 className="font-bold text-2xl">Persoonlijke AI-assistent</h1>
-          <p>I ❤️ Windesheim</p>
+          <p>I ❤️ Windesheim!</p>
       </div>
   );
 }
