9b8500919bd2a03d407f8250e91480cd37b29be1
Thomas Michiels <thomasmichiels0@gmail.com>
2025-04-09 18:20:59 +0200

    Test for deployment

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,23 +1,40 @@
+// import { ReactNode } from 'react'
+// import { notFound } from 'next/navigation'
+// import { NextIntlClientProvider } from 'next-intl'
+// import Header from '@/components/Header'
+//
+// interface LocaleLayoutProps {
+//     children: ReactNode
+//     params: { locale: string }
+// }
+//
+// export default async function LocaleLayout({ children, params }: LocaleLayoutProps) {
+//     let messages
+//     try {
+//         messages = (await import(`@/messages/${params.locale}.json`)).default
+//     } catch (error) {
+//         notFound()
+//     }
+//
+//     return (
+//         <NextIntlClientProvider locale={params.locale} messages={messages}>
+//             <Header locale={params.locale} />
+//             {children}
+//         </NextIntlClientProvider>
+//     )
+// }
 import { ReactNode } from 'react'
-import { notFound } from 'next/navigation'
 import { NextIntlClientProvider } from 'next-intl'
 import Header from '@/components/Header'
 
-interface LocaleLayoutProps {
+type Props = {
     children: ReactNode
     params: { locale: string }
 }
 
-export default async function LocaleLayout({ children, params }: LocaleLayoutProps) {
-    let messages
-    try {
-        messages = (await import(`@/messages/${params.locale}.json`)).default
-    } catch (error) {
-        notFound()
-    }
-
+export default function LocaleLayout({ children, params }: Props) {
     return (
-        <NextIntlClientProvider locale={params.locale} messages={messages}>
+        <NextIntlClientProvider locale={params.locale}>
             <Header locale={params.locale} />
             {children}
         </NextIntlClientProvider>
