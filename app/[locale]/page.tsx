7ae7216936feceb47fe6e743166d9e086e956507
Thomas Michiels <thomasmichiels0@gmail.com>
2025-04-11 13:43:24 +0200

    Forgot password

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,27 @@
 'use client'
 
 import { useTranslations } from 'next-intl'
+import { useEffect } from 'react'
+import { useRouter } from '@/i18n/navigation'
 
 export default function LocalePage() {
     const t = useTranslations()
+    const router = useRouter()
+
+    useEffect(() => {
+        const hash = window.location.hash
+        const params = new URLSearchParams(hash.slice(1))
+        const type = params.get('type')
+
+        if (type === 'signup') {
+            router.replace('/login?confirmed=true')
+        }
+
+        if (type === 'recovery') {
+            router.replace('/reset-password-confirm' + window.location.hash)
+        }
+    }, [])
+
 
     return (
         <main className="p-6 text-center">
