91b328b1d27e797fc1e8abfee3298a141ab12dd0
Thomas Michiels <thomasmichiels0@gmail.com>
2025-04-11 10:21:38 +0200

    Test for deployment!!!

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -24,17 +24,25 @@
 //     )
 // }
 import { ReactNode } from 'react'
+import { notFound } from 'next/navigation'
 import { NextIntlClientProvider } from 'next-intl'
 import Header from '@/components/Header'
 
-type Props = {
+interface LocaleLayoutProps {
     children: ReactNode
     params: { locale: string }
 }
 
-export default function LocaleLayout({ children, params }: Props) {
+export default async function LocaleLayout({ children, params }: LocaleLayoutProps) {
+    let messages
+    try {
+        messages = (await import(`@/messages/${params.locale}.json`)).default
+    } catch (error) {
+        notFound()
+    }
+
     return (
-        <NextIntlClientProvider locale={params.locale}>
+        <NextIntlClientProvider locale={params.locale} messages={messages}>
             <Header locale={params.locale} />
             {children}
         </NextIntlClientProvider>
