0b2f1c69e9adf90b4d88c29dfe36c811556a05a9
Thomas Michiels <thomasmichiels0@gmail.com>
2025-04-11 11:00:43 +0200

    AAAAAAAAAAAA

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,16 +1,23 @@
-import { ReactNode } from 'react'
-import { notFound } from 'next/navigation'
-import { NextIntlClientProvider } from 'next-intl'
+import {ReactNode} from 'react'
+import {notFound} from 'next/navigation'
+import {NextIntlClientProvider} from 'next-intl'
 import Header from '@/components/Header'
 
-interface LocaleLayoutProps {
-    children: ReactNode
-    params: { locale: string }
-    // ✨ voeg een index signature toe om foute "parallel segment props" te negeren
-    [key: string]: unknown
-}
-
-export default async function LocaleLayout({ children, params }: LocaleLayoutProps) {
+// interface LocaleLayoutProps {
+//     children: ReactNode
+//     params: { locale: string }
+//     // ✨ voeg een index signature toe om foute "parallel segment props" te negeren
+//     [key: string]: unknown
+// }
+//
+// export default async function LocaleLayout({ children, params }: LocaleLayoutProps) {
+export default async function LocaleLayout({
+                                               children,
+                                               params,
+                                           }: {
+    children: React.ReactNode
+    params: { [key: string]: string }
+}) {
     let messages
     try {
         messages = (await import(`@/messages/${params.locale}.json`)).default
@@ -20,7 +27,7 @@ export default async function LocaleLayout({ children, params }: LocaleLayoutPro
 
     return (
         <NextIntlClientProvider locale={params.locale} messages={messages}>
-            <Header locale={params.locale} />
+            <Header locale={params.locale}/>
             {children}
         </NextIntlClientProvider>
     )
